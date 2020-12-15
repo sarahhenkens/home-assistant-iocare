0322929eeb68931863e2abfe79ddcd7aff934060
kylerw <kylerw@gmail.com>
2020-12-15 06:57:06 -0700

    Update __init__.py

--- custom_components/iocare/__init__.py
+++ custom_components/iocare/__init__.py
@@ -40,5 +40,8 @@ async def async_setup_entry(hass, config_entry):
     hass.async_add_job(
         hass.config_entries.async_forward_entry_setup(config_entry, "air_quality")
     )
+    hass.async_add_job(
+        hass.config_entries.async_forward_entry_setup(config_entry, "switch")
+    )
 
     return True
