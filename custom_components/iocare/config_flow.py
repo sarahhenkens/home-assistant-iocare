4f8042f09022e058002009bab6f0f6025ac0a4f9
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-03-23 13:49:43 -0400

    Add error handling and aborting to config_flow
    Rewritten to include testing user provided credentials for successful log in ability. If unable to log in with user provided credentials, user is notified on the integration screen. Also includes check to make sure only one instance of the integration is installed at a time. Attempting to add a second instance of IOCare will abort the attempt and show an appropriate message to the end user.

--- custom_components/iocare/config_flow.py
+++ custom_components/iocare/config_flow.py
@@ -1,33 +1,79 @@
 """Config flow of our component"""
-
+import asyncio
+import logging
 import voluptuous as vol
-from homeassistant import config_entries
+from iocare.iocareapi import IOCareApi
+from homeassistant import config_entries, exceptions
+from homeassistant.core import callback
 from homeassistant.const import (
     CONF_PASSWORD,
     CONF_USERNAME
 )
 from .const import DOMAIN
+_LOGGER = logging.getLogger(__name__)
 
-
-@config_entries.HANDLERS.register(DOMAIN)
-class IoCareConfigFlow(config_entries.ConfigFlow):
+class IoCareConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle our config flow."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
-    async def async_step_user(self, user_input=None):
-        """Handle a flow start."""
-
-        fields = vol.Schema({
+    def __init__(self):
+        """Initialize IOCare configuration flow"""
+        self.schema = vol.Schema({
             vol.Required(CONF_USERNAME): str,
             vol.Required(CONF_PASSWORD): str
         })
 
-        if user_input is not None:
-            return self.async_create_entry(
-                title="Coway IoCare Config Entry",
-                data=user_input,
-            )
+        self._username = None
+        self._password = None
 
-        return self.async_show_form(step_id="user", data_schema=fields)
+    async def async_step_user(self, user_input=None):
+        """Handle a flow start."""
+
+        if self._async_current_entries():
+            return self.async_abort(reason="already_configured")
+
+        if not user_input:
+            return self._show_form()
+
+        self._username = user_input[CONF_USERNAME]
+        self._password = user_input[CONF_PASSWORD]
+
+        return await self._async_iocare_login()
+
+
+    async def _async_iocare_login(self):
+
+        errors = {}
+
+        try:
+            client = await self.hass.async_add_executor_job(IOCareApi, self._username, self._password)
+            await self.hass.async_add_executor_job(client.check_access_token)
+
+        except Exception:
+            _LOGGER.error("Unable to connect to IOCare: Failed to Log In")
+            errors = {"base": "auth_error"}
+
+        if errors:
+            return self._show_form(errors=errors)
+
+        return await self._async_create_entry()
+
+    async def _async_create_entry(self):
+        """Create the config entry."""
+        config_data = {
+            CONF_USERNAME: self._username,
+            CONF_PASSWORD: self._password,
+        }
+
+        return self.async_create_entry(title="Coway IOCare Config Entry", data=config_data)
+
+    @callback
+    def _show_form(self, errors=None):
+        """Show the form to the user."""
+        return self.async_show_form(
+            step_id="user",
+            data_schema=self.schema,
+            errors=errors if errors else {},
+        )
